8deca61bdf8ac7d9844f62080c20e3eba3392ed7
Tobias de Vargas <42094615+TobiasVargas@users.noreply.github.com>
2020-06-19 09:29:38 -0300

    API Gerada automaticamente pelo Visual Studio 2019

---
+++ CrudApiDefault/Data/CrudApiDefaultContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CrudApiDefault.Models;
+
+namespace CrudApiDefault.Data
+{
+    public class CrudApiDefaultContext : DbContext
+    {
+        public CrudApiDefaultContext (DbContextOptions<CrudApiDefaultContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CrudApiDefault.Models.Product> Product { get; set; }
+    }
+}
---
+++ CrudApiDefault/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudApiDefault.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public int Estoque { get; set; }
+        [Required]
+        public float Preco { get; set; }
+    }
+}
